3e3f4db4ca253ada1f5ec4c326cc06e4743ef9e9
Ahmad Alifariz Anwar <hanaaraashi@gmail.com>
2025-06-20 16:06:50 +0800

    menambahkan fungsi ke manajemen buku

--- perpus.cpp
+++ perpus.cpp
@@ -2,7 +2,6 @@
 #include <string>
 using namespace std;
 
-
 struct buku{
     int id;
     string judul;
@@ -13,12 +12,142 @@ struct buku{
 };
 buku *headBook = nullptr;
 
-void manajemenBuku();
+void tambahBuku(){
+    buku *BukuBaru = new buku();
+    cout<<"+========================================+"<<endl;
+    cout<<"    TAMBAH BUKU BARU     "<<endl;
+    cout<<"+========================================+"<<endl;
+    cout<<"Masukkan ID buku: ";cin>>BukuBaru->id;
+    cout<<"Masukkan Judul Buku: ";getline(cin >> ws, BukuBaru->judul);
+    cout<<"Masukkan Penulis: ";getline(cin, BukuBaru->penulis);
+    cout<<"Masukkan tahun terbit: ";cin>>BukuBaru->tahun;
+    BukuBaru->tersedia = true;
+    BukuBaru->next = nullptr;
+
+    if(headBook == nullptr){
+        headBook = BukuBaru;
+    }else{
+        buku *temp = headBook;
+        while(temp->next != nullptr){
+            temp = temp->next;
+        }
+        temp->next = BukuBaru;
+    }
+    cout<<"Buku baru berhasil ditambahkan"<<endl;
+}
+
+void tampilBuku(){
+    if(headBook == nullptr){
+        cout<<"LIST BUKU KOSONG!"<<endl;
+        cout<<"Silahkan tambahkan buku terlebih dahulu."<<endl;
+        cout << "Tekan Enter untuk kembali...";
+        cin.ignore();
+        cin.get();
+        system("cls");
+        return;
+    }
+
+    cout<<"+========================================+"<<endl;
+    cout<<"     DAFTAR BUKU     "<<endl;
+    cout<<"+========================================+"<<endl;
+
+    buku *temp = headBook;
+    while(temp != nullptr){
+        cout<<"ID: "<<temp->id<<endl;
+        cout<<"Judul: "<<temp->judul<<endl;
+        cout<<"Penulis: "<<temp->penulis<<endl;
+        cout<<"Tahun: "<<temp->tahun<<endl;
+        cout<<"Status: "<<(temp->tersedia ? "tersedia" : "Dipinjam")<<endl;
+        cout<<"------------------------"<<temp->id<<endl;
+        temp = temp->next;
+    }
+    cout << "Tekan Enter untuk kembali...";
+    cin.ignore();
+    cin.get();
+    system("cls");
+}
+
+void hapusBuku(){
+    int targetID;
+    if(headBook == nullptr){
+        cout<<"LIST BUKU KOSONG!"<<endl;
+        cout<<"Silahkan tambahkan buku terlebih dahulu."<<endl;
+        cout << "Tekan Enter untuk kembali...";
+        cin.ignore();
+        cin.get();
+        system("cls");
+        return;
+    }
+
+    cout<<"+========================================+"<<endl;
+    cout<<"    HAPUS BUKU     "<<endl;
+    cout<<"+========================================+"<<endl;
+
+    cout<<"Masukkan ID buku yang ingin dihapus: ";cin>>targetID;
+    buku *temp = headBook;
+    buku *prev = nullptr;
+    while(temp != nullptr && temp->id != targetID){
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if(temp == nullptr){
+        cout<<"Buku dengan ID tersebut tidak ditemukan."<<endl;
+        return;
+    }
+
+    if(prev == nullptr){
+        headBook = temp->next;
+    }
+    else
+    {
+        prev->next = temp->next;
+    }
+    delete temp;
+    cout<<"Buku berhasil dihapus."<<endl;
+}
+
+void manajemenBuku(){
+    int pilihan;
+    do{
+        cout<<"+========================================+"<<endl;
+        cout<<"    MENU MANAJEMEN BUKU     "<<endl;
+        cout<<"+========================================+"<<endl;
+        cout<<""<<endl;
+
+        cout<<"1. Tambah Buku"<<endl;
+        cout<<"2. Tampilkan Daftar Buku"<<endl;
+        cout<<"3. Hapus Buku"<<endl;
+        cout<<"4. Kembali ke menu Utama"<<endl;
+        cout<<"Pilih: ";
+        cin>>pilihan;
+        
+        system("cls");
+        switch(pilihan){
+            case 1:
+                tambahBuku();
+                break;
+            case 2:
+                tampilBuku();
+                break;
+            case 3:
+                hapusBuku();
+                break;
+            case 4:
+                cout<<"Kembali ke menu Utama"<<endl;break;
+            default:
+                cout<<"Pilihan tidak valid";
+        }
+    }while(pilihan != 4);
+
+}
+
+
 void menuUtama(){
     int pilihan;
     do{
         cout<<"+========================================+"<<endl;
-        cout<<"\n    SISTEM PERPUSTAKAAN    "<<endl;
+        cout<<"    SISTEM PERPUSTAKAAN    "<<endl;
         cout<<"+========================================+"<<endl;
         cout<<""<<endl;
 
@@ -30,8 +159,8 @@ void menuUtama(){
         cout<<"6. Keluar"<<endl;
         cout<<"Pilih menu: ";
         cin>>pilihan;
-        system("cls");
 
+        system("cls");
         switch(pilihan){
             case 1:
                 manajemenBuku();
@@ -51,44 +180,9 @@ void menuUtama(){
     }while(pilihan != 6);
 }
 
-void manajemenBuku(){
 
-    int pilihan;
 
-    do{
-        cout<<"+========================================+"<<endl;
-        cout<<"\n    MENU MANAJEMEN BUKU     "<<endl;
-        cout<<"+========================================+"<<endl;
-        cout<<""<<endl;
-
-        cout<<"1. Tambah Buku"<<endl;
-        cout<<"2. Tampilkan Daftar Buku"<<endl;
-        cout<<"3. Hapus Buku"<<endl;
-        cout<<"4. Kembali ke menu Utama"<<endl;
-        cout<<"Pilih: ";
-        cin>>pilihan;
-        system("cls");
-
-        switch(pilihan){
-
-            case 1:
-                break;
-
-            case 2:
-                break;
-
-            case 3:
-                break;
-
-            case 4:
-                cout<<"Kembali ke menu Utama"<<endl;
-                break;
-
-            default:
-                cout<<"Pilihan tidak valid";
-
-        }
-
-    }while(pilihan!=4);
-
+int main(){
+    menuUtama();
+    return 0;
 }
