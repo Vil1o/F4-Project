a7f10f6b2048ab21c177ba5979a76704e53e4f09
Ahmad Alifariz Anwar <hanaaraashi@gmail.com>
2025-06-22 15:39:55 +0800

    memperbaiki penghapusan data ketika ada peminjaman

--- perpus.cpp
+++ perpus.cpp
@@ -127,6 +127,20 @@ void hapusBuku(){
         system("cls");
         return;
      }
+
+    peminjaman* cek = headPeminjaman;
+    while(cek != nullptr){
+        if(cek->idBuku == targetID){
+            cout << "Buku ini sedang dipinjam dan tidak bisa dihapus!" << endl;
+            cout << "Tekan Enter untuk kembali..." << endl;
+            cin.ignore();
+            cin.get();
+            system("cls");
+            return;
+        }
+        cek = cek->next;
+    }
+
     buku *temp = headBook;
     buku *prev = nullptr;
     while(temp != nullptr && temp->id != targetID){
@@ -282,6 +296,20 @@ void hapusAnggota(){
         system("cls");
         return;
      }
+
+     peminjaman* cek = headPeminjaman;
+    while(cek != nullptr){
+        if(cek->idAnggota == targetID){
+            cout << "Anggota ini sedang meminjam buku dan tidak bisa dihapus!" << endl;
+            cout << "Tekan Enter untuk kembali..." << endl;
+            cin.ignore();
+            cin.get();
+            system("cls");
+            return;
+        }
+        cek = cek->next;
+    }
+
     anggota *temp = headAnggota;
     anggota *prev = nullptr;
     while(temp != nullptr && temp->id != targetID){
